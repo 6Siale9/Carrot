9690181ce41efd3e3d7dae77ce88e2a619760761
Titdess <titouan.dessibourg@gmail.com>
2026-06-27 00:54:43 +0200

    Bouton Sélection mode + changement cursor

---
+++ Assets/clickModeController.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class clickModeController : MonoBehaviour
+{
+    [SerializeField] private ModeOfCursor modeOfCursor;
+
+
+    public void carrotButton()
+    {
+        cursorController.Instance.setToMode(ModeOfCursor.Carrot);
+    }
+
+    public void defaultButton()
+    {
+        cursorController.Instance.setToMode(ModeOfCursor.Default);
+    }
+
+    public void stickButton()
+    {
+        cursorController.Instance.setToMode(ModeOfCursor.Stick);
+    }
+}
---
+++ Assets/cursorController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class cursorController : MonoBehaviour
+{
+    public static cursorController Instance { get; private set; }
+
+    [SerializeField] private Texture2D cursorTextureDefault;
+    [SerializeField] private Texture2D cursorTextureCarrot;
+    [SerializeField] private Texture2D cursorTextureStick;
+
+    [SerializeField] private Vector2 clickPosition = Vector2.zero;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorTextureDefault, clickPosition, CursorMode.Auto);
+    }
+
+
+
+
+
+    public void setToMode(ModeOfCursor modeOfCursor)
+    {
+        switch (modeOfCursor)
+        {
+            case ModeOfCursor.Default:
+                Cursor.SetCursor(cursorTextureDefault, clickPosition, CursorMode.Auto);
+                break;
+            case ModeOfCursor.Carrot:
+                Cursor.SetCursor(cursorTextureCarrot, clickPosition, CursorMode.Auto);
+                break;
+            case ModeOfCursor.Stick:
+                Cursor.SetCursor(cursorTextureStick, clickPosition, CursorMode.Auto);
+                break;
+            default:
+                Cursor.SetCursor(cursorTextureDefault, clickPosition, CursorMode.Auto);
+                break;
+        }
+    }
+
+
+
+
+
+
+
+}
+ public enum ModeOfCursor
+{
+     Default,
+    Carrot,
+    Stick
+}
